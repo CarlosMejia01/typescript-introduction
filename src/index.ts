13a6baf427eb3ef09ecb2607225d0af366100f56
cmejia <cmejia@equitel.com.co>
2023-01-06 06:42:38 -0500

    feat: type of ts and js

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,35 @@ let mensaje: string = "Hola Mundo";
 
 mensaje = "Nuevo Hola Mundo";
 
+mensaje = "Nuevo Mundo";
 console.log(mensaje);
 
-mensaje = "Nuevo Mundo";
+console.log(typeof []);
+
+/**
+ * Tipos de JS
+ * number
+ * string
+ *  boolean
+ *  null
+ *  undefined
+ *  object
+ *  function
+ *
+ *  Tipos de TS
+ *  any => warning
+ *  unknown
+ *  never
+ *  arrays
+ *  tuplas
+ *  enums
+ *
+ * Tipos Inferidos
+ *
+ */
+
+let extincionDinosaurios: number = 76_000_000;
+let dinosaurioFavorito: string = "T-Rex";
+let extintos: boolean = true;
+
+let variable; //any
